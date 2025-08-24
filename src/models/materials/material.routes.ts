7772ea96336dd2ef496443c2c9719f1f4e1080ca
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-08-24 14:09:15 +0200

    chore: protect material write routes with simple admin token

--- src/models/materials/material.routes.ts
+++ src/models/materials/material.routes.ts
@@ -6,13 +6,14 @@ import {
   updateMaterialById,
   deleteMaterialById,
 } from "./material.controller";
+import { adminGuard } from "../../middleware/adminGuard";
 
 const router = Router();
 
 router.get("/", listMaterials);
 router.get("/:type/:slug", getMaterialByTypeSlug);
-router.post("/", createMaterial);
-router.put("/:id", updateMaterialById);     
-router.delete("/:id", deleteMaterialById);  
+router.post("/", adminGuard, createMaterial);
+router.put("/:id", adminGuard, updateMaterialById);     
+router.delete("/:id", adminGuard, deleteMaterialById);  
 
 export default router;
