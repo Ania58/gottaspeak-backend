442a6488302ae184075ed56ebf333003ea6a724a
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-10-19 19:10:13 +0200

    feat(sessions): expose GET /sessions/:id/join for magic-link entry

--- src/sessions/session.routes.ts
+++ src/sessions/session.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createSession, getSession } from "./session.controller";
 import { createInviteLink } from "./session.invite.controller";
+import { joinSession } from "./session.join.controller";
 
 const router = Router();
 
@@ -9,5 +10,7 @@ router.post("/", createSession);
 router.get("/:id", getSession);
 
 router.post("/invite", createInviteLink);
+
+router.get("/:id/join", joinSession);
 
 export default router;
